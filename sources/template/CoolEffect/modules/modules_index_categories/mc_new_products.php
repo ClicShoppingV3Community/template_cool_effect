d91924575b92fbd327151af50a648b6da7bf0c46
ClicShopping <Yae8mr2o*>
2020-06-25 09:38:45 +0200

    update template

--- sources/template/CoolEffect/modules/modules_index_categories/mc_new_products.php
+++ sources/template/CoolEffect/modules/modules_index_categories/mc_new_products.php
@@ -293,7 +293,7 @@
                 $tag = $CLICSHOPPING_ProductsFunctionTemplate->getProductsHeadTag($products_id);
 
                 $products_tag = '';
-                if (!is_null($tag)) {
+                if (is_array($tag)) {
                   foreach ($tag as $value) {
                     $products_tag .= '#<span class="productTag">' . HTML::link(CLICSHOPPING::link(null, 'Search&keywords='. HTML::outputProtected(utf8_decode($value) .'&search_in_description=1&categories_id=&inc_subcat=1'), 'rel="nofollow"'), $value) . '</span> ';
                   }
@@ -371,7 +371,7 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Do you want display the title ?',
+          'configuration_title' => 'Do you want to display the title ?',
           'configuration_key' => 'MODULE_INDEX_CATEGORIES_NEW_PRODUCTS_FRONT_TITLE',
           'configuration_value' => 'True',
           'configuration_description' => 'Display the title',
@@ -386,7 +386,7 @@
           'configuration_title' => 'Please indicate the number to display',
           'configuration_key' => 'MODULE_INDEX_CATEGORIES_NEW_PRODUCTS_MAX_DISPLAY',
           'configuration_value' => '6',
-          'configuration_description' => 'VIndicate the number to display.',
+          'configuration_description' => 'Indicate the number to display.',
           'configuration_group_id' => '6',
           'sort_order' => '5',
           'set_function' => '',
@@ -407,7 +407,7 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Do you want display a short description ?',
+          'configuration_title' => 'Do you want to display a short description ?',
           'configuration_key' => 'MODULE_INDEX_CATEGORIES_NEW_PRODUCTS_SHORT_DESCRIPTION',
           'configuration_value' => '0',
           'configuration_description' => 'Please indicate a number of your short description',
@@ -419,7 +419,7 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Do you want remove words of your short description ? ',
+          'configuration_title' => 'Do you want to remove words of your short description ? ',
           'configuration_key' => 'MODULE_INDEX_CATEGORIES_NEW_PRODUCTS_SHORT_DESCRIPTION_DELETE_WORLDS',
           'configuration_value' => '0',
           'configuration_description' => 'Please indicate a number',
@@ -431,7 +431,7 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Do you want display a message News / Specials / Favorites / Featured ?',
+          'configuration_title' => 'Do you want to display a message News / Specials / Favorites / Featured ?',
           'configuration_key' => 'MODULE_INDEX_CATEGORIES_NEW_PRODUCTS_TICKER',
           'configuration_value' => 'False',
           'configuration_description' => 'Display a message News / Specials / Favorites / Featured',
@@ -443,7 +443,7 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Do you want display the discount pourcentage (specials) ?',
+          'configuration_title' => 'Do you want to display the discount pourcentage (specials) ?',
           'configuration_key' => 'MODULE_INDEX_CATEGORIES_NEW_PRODUCTS_POURCENTAGE_TICKER',
           'configuration_value' => 'False',
           'configuration_description' => 'Display the discount pourcentage (specials)',
@@ -455,7 +455,7 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Do you want display the stock ?',
+          'configuration_title' => 'Do you want to display the stock ?',
           'configuration_key' => 'MODULE_INDEX_CATEGORIES_NEW_PRODUCTS_DISPLAY_STOCK',
           'configuration_value' => 'none',
           'configuration_description' => 'Display the stock (in stock, exhaused, out of stock) ?',
@@ -479,7 +479,7 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Do you want remove the details button ?',
+          'configuration_title' => 'Do you want to remove the details button ?',
           'configuration_key' => 'MODULE_INDEX_CATEGORIES_NEW_PRODUCTS_DELETE_BUY_BUTTON',
           'configuration_value' => 'False',
           'configuration_description' => 'Remove the button details',
--- sources/template/CoolEffect/modules/modules_products_listing/pl_products_listing.php
+++ sources/template/CoolEffect/modules/modules_products_listing/pl_products_listing.php
@@ -251,7 +251,7 @@
                 $tag = $CLICSHOPPING_ProductsFunctionTemplate->getProductsHeadTag($products_id);
 
                 $products_tag = '';
-                if (!is_null($tag)) {
+                if (is_array($tag)) {
                   foreach ($tag as $value) {
                     $products_tag .= '#<span class="productTag">' . HTML::link(CLICSHOPPING::link(null, 'Search&keywords='. HTML::outputProtected(utf8_decode($value) .'&search_in_description=1&categories_id=&inc_subcat=1'), 'rel="nofollow"'), $value) . '</span> ';
                   }
@@ -375,7 +375,7 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Do you want display a short description ?',
+          'configuration_title' => 'Do you want to display a short description ?',
           'configuration_key' => 'MODULE_PRODUCTS_LISTING_SHORT_DESCRIPTION',
           'configuration_value' => '0',
           'configuration_description' => 'Please indicate a number of your short description',
@@ -387,7 +387,7 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Do you want remove words of your short description ?',
+          'configuration_title' => 'Do you want to remove words of your short description ?',
           'configuration_key' => 'MODULE_PRODUCTS_LISTING_SHORT_DESCRIPTION_DELETE_WORLDS',
           'configuration_value' => '0',
           'configuration_description' => 'Please indicate a number',
@@ -399,7 +399,7 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Do you want display a message News / Specials / Favorites / Featured ?',
+          'configuration_title' => 'Do you want to display a message News / Specials / Favorites / Featured ?',
           'configuration_key' => 'MODULE_PRODUCTS_LISTING_TICKER',
           'configuration_value' => 'False',
           'configuration_description' => 'Display a message News / Specials / Favorites / Featured',
@@ -411,7 +411,7 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Do you want display the discount pourcentage (specials) ?',
+          'configuration_title' => 'Do you want to display the discount pourcentage (specials) ?',
           'configuration_key' => 'MODULE_PRODUCTS_LISTING_POURCENTAGE_TICKER',
           'configuration_value' => 'False',
           'configuration_description' => 'Display the discount pourcentage (specials)',
@@ -423,7 +423,7 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Do you want display the stock ?',
+          'configuration_title' => 'Do you want to display the stock ?',
           'configuration_key' => 'MODULE_PRODUCTS_LISTING_DISPLAY_STOCK',
           'configuration_value' => 'none',
           'configuration_description' => 'Display the stock (in stock, exhaused, out of stock) ?',
@@ -447,7 +447,7 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Do you want remove the details button ?',
+          'configuration_title' => 'Do you want to remove the details button ?',
           'configuration_key' => 'MODULE_PRODUCTS_LISTING_DELETE_BUY_BUTTON',
           'configuration_value' => 'False',
           'configuration_description' => 'Remove the button details',
